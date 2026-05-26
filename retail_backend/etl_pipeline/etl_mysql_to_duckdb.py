cf30006de152312f13c34e677396ce30e90b4333
moktagithub005 <moktaajay@gmail.om>
2026-05-26 22:10:21 +0530

    add all new changes

--- retail_backend/etl_pipeline/etl_mysql_to_duckdb.py
+++ retail_backend/etl_pipeline/etl_mysql_to_duckdb.py
@@ -1,7 +1,40 @@
 import pandas as pd 
 from sqlalchemy import create_engine
 
-mysql_engine = create_engine(
+mysql_engine = create_engine("mysql+pymysql://root:MacMYSQL005@localhost/retail_oltp")
 
-    "mysql+pymysql://root:YOUR_PASSWORD@localhost/retail_oltp"
-)
+query= "SELECT * FROM online_retail"
+
+df = pd.read_sql(query, mysql_engine)
+print(df.head())
+
+
+import duckdb
+#duck_conn =  duckdb.connect("retail_analytics.duckdb")
+duck_conn =  duckdb.connect("md:")
+
+
+# load data into duckdb 
+
+duck_conn.execute( """
+
+     CREATE TABLE IF NOT EXISTS olap_retail AS
+     SELECT *  FROM df 
+
+""")
+
+# verify data inside duckdb 
+
+result = duck_conn.execute( """
+      SELECT *
+      FROM olap_retrail
+      LIMIT 5
+
+""").fetchdf()
+
+print(result)
+
+## create cloud database 
+
+
+
