75c5e8561894342593b27091bce5ca7b85472b86
moktagithub005 <moktaajay@gmail.om>
2026-06-29 21:47:36 +0530

    add all new changes

--- weather_streaming_platform/producer/weather_producer.py
+++ weather_streaming_platform/producer/weather_producer.py
@@ -1,2 +1,33 @@
-from kafka_producer import publish_and_confirmation
+import json
+import time
+import logging
 from weather_service import get_weather_event
+from kafka_producer import(
+    publish_and_confirmation,
+    close
+)
+
+def run_weather_producer():
+    while True:
+        try:
+            event=get_weather_event()
+            metadata=publish_and_confirmation(
+                event,
+                key=event["city"]
+            )
+            logging.info(
+                f"""
+published sucessfully
+Topic: {metadata.topic}
+Partition: {metadata.partition}
+Offset: {metadata.offset}
+"""
+     )
+        except Exception as e:
+            logging.exception(
+                f"producer Error:{e}"
+            )
+        finally:
+            time.sleep(30)
+        
+
