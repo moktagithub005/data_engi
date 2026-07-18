a42650ab774e42a70da7606708e5fcb0520426b0
moktagithub005 <moktaajay@gmail.om>
2026-07-18 21:48:24 +0530

    add all new changes

--- weather_streaming_platform/configs/mysql_config.py
+++ weather_streaming_platform/configs/mysql_config.py
@@ -2,9 +2,11 @@
 
 MYSQL_HOST = "localhost"
 
-MYSQL_USER = "root"
-
-MYSQL_PASSWORD = "root"
+MYSQL_PORT = 3306
 
 MYSQL_DATABASE = "weather_db"
+
+MYSQL_USERNAME = "root"
+
+MYSQL_PASSWORD = "@MacMYSQL005"
 
